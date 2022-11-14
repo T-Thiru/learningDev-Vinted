f61a78b6c74c1b85b0ee48dfa1d2f6cfd2d4d7e5
Thiruvaran Thayalan <110049957+T-Thiru@users.noreply.github.com>
2022-11-14 12:11:50 +0100

    modified condition signup routees

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,67 +15,55 @@ const convertToBase64 = (file) => {
 router.post("/user/signup", fileUpload(), async (req, res) => {
   try {
     console.log(req.body);
-    const { username, email, password, newsletter } = req.body;
+    console.log(req.files?.picture);
+    const user = await User.findOne({ email: req.body.email });
+    const avatar = req.files?.picture;
 
-    if (validator.isEmail(email)) {
-      const checkUser = await User.findOne({ email: email });
-      //console.log(checkUser);
-      if (checkUser) {
-        return res.json({
-          message: "email already used",
-        });
-      }
-      if (!username) {
-        return res.json({
-          message: "please enter username",
-        });
-      }
-
-      const salt = uid2(16);
-      const hash = SHA256(password + salt).toString(encBase64);
-      const token = uid2(16);
-
-      const user = new User({
-        email: email,
-        account: {
-          username: username,
-        },
-        newsletter: newsletter,
-        token: token,
-        hash: hash,
-        salt: salt,
-      });
-
-      if (req.files?.picture) {
-        const avatarToBe = await cloudinary.uploader.upload(
-          convertToBase64(req.files.picture),
-          { folder: `/vinted/users/${user._id}` }
-        );
-
-        Object.assign(user.account, {
-          avatar: {
-            picture: avatarToBe,
-          },
-        });
-      }
-
-      await user.save();
-      res.json({
-        id: user.id,
-        token: user.token,
-        account: {
-          username: user.account.username,
-          avatar: {
-            picture: user.account.avatar.picture,
-          },
-        },
-      });
+    if (user) {
+      res.status(409).json({ message: "This email already has an account" });
     } else {
-      res.json({
-        error: "invalid email adress",
-      });
+      if (req.body.email && req.body.password && req.body.username) {
+        const token = uid2(64);
+        const salt = uid2(64);
+        const hash = SHA256(req.body.password + salt).toString(encBase64);
+
+        const newUser = new User({
+          email: req.body.email,
+          token: token,
+          hash: hash,
+          salt: salt,
+          account: {
+            username: req.body.username,
+          },
+          newsletter: req.body.newsletter,
+        });
+
+        if (avatar) {
+          const avatarToBe = await cloudinary.uploader.upload(
+            convertToBase64(avatar),
+            { folder: `/vinted/users/${newUser._id}` }
+          );
+
+          Object.assign(newUser.account, {
+            avatar: {
+              picture: avatarToBe,
+            },
+          });
+        }
+
+        await newUser.save();
+        res.status(200).json({
+          _id: newUser._id,
+          email: newUser.email,
+          token: newUser.token,
+          account: newUser.account,
+        });
+      } else {
+        res.status(400).json({ message: "Missing parameters" });
+      }
     }
   } catch (error) {
+    console.log(error.message);
     res.status(400).json({ message: error.message });
   }
 });
